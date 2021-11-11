48e8d93a20c7e83aed0156f401e11ba3fb575dab
xiaoaoran <34958050+xiaoaoran@users.noreply.github.com>
2021-11-11 11:19:23 +0800

    Rename 1.sh to train.sh

--- train/tasks/semantic/1.sh
+++
@@ -1,1 +0,0 @@
-python train.py -d /data/SemanticKITTI/dataset --arch_cfg config/arch/arch_cfg.yaml --data_cfg config/labels/semantic-kitti.yaml / --gpu 0 1
---
+++ train/tasks/semantic/train.sh
@@ -0,0 +1,1 @@
+python train.py -d /data/SemanticKITTI/dataset --arch_cfg config/arch/arch_cfg.yaml --data_cfg config/labels/semantic-kitti.yaml / --gpu 0 1
