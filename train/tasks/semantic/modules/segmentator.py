5a0906aaad85ac6888259d8e56227518696e0834
Xiao Aoran <34958050+xiaoaoran@users.noreply.github.com>
2022-09-19 13:06:17 +0800

    Update segmentator.py

--- train/tasks/semantic/modules/segmentator.py
+++ train/tasks/semantic/modules/segmentator.py
@@ -177,7 +177,8 @@ class Segmentator(nn.Module):
       assert(mask is not None)
       y = self.CRF(x, y, mask)
 
-   return y, range, zxy, remission
+   return y  # , range, zxy, remission
+   # return y, range, zxy, remission
 
   def save_checkpoint(self, logdir, suffix=""):
     # Save the weights
